d982ace69dca0a7a7ab88860f558b59a7ab6c7a9
JULIOMOLINA1 <juliocesarmolinacabezas@gmail.com>
2025-10-31 21:28:58 -0500

    que es var variable

---
+++ src/main/java/pe/edu/tecsup/app/VarJava.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup.app;
+
+public class VarJava {
+    public static void main(String[] args) {
+        var nombre = "Informaticonfig"; //You have to initilize var variable, has to be done, absolutely
+        var numero = 20;
+        var cifra = 10.50;
+
+        System.out.println(nombre);
+    }
+}
