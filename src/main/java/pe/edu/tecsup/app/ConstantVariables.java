f5634a89282e678f5ec5bd46fcaf3ace8ceac0b1
JULIOMOLINA1 <juliocesarmolinacabezas@gmail.com>
2025-11-01 18:41:00 -0500

    Define constant variables

---
+++ src/main/java/pe/edu/tecsup/app/ConstantVariables.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+public class ConstantVariables {
+
+    public static void main(String[] args) {
+
+        final double gravity = 9.8;
+        final double pi = 3.14;
+        System.out.println(gravity);
+        System.out.println(pi);
+
+    }
+}
